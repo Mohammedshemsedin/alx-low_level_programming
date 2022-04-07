bdc3a3ffe98ab9218ec03b292a7943218a826352
mohammed shemsedin <mohammedshemsedin90@gmail.com>
2022-04-07 07:12:56 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 
 /**
  * Use the function printf -with proper grammar,
- *but the outcome is a piece of art,
- * 
- *\
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
