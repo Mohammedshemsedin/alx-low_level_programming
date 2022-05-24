50807505a3a75c60f646d5f111e9615abd591f01
mohammedshemsedin <mohammedshemsedin900@gmail.com>
2022-05-23 23:05:44 -0700

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,16 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int nodes = 0;
+	int nodes;
 
+	nodes = 0;
 	while (h)
 	{
+
 		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
+
 	}
 
 	return (nodes);
